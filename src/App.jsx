566823247683668fd427ce61c5bab28a94e8bfc5
Jean Willian de Souza Ferreira <jeansferreira6@gmail.com>
2024-09-29 12:02:15 -0300

    Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,128 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectProjectId: undefined,
+    projects: [],
+    tasks: [],
+  });
+
+  // Ficar passando um método ou valor por vários componentes até chegar no desejado se chama "pop drilling"
+  const handleAddTask = (text) => {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  };
+
+  const handleDeleteTask = (id) => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  };
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectProjectId: null,
+      };
+    });
+  };
+
+  const handleCancelAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectProjectId: undefined,
+      };
+    });
+  };
+
+  const handleAddProject = (projectData) => {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  const handleSelectProject = (id) => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectProjectId: id,
+      };
+    });
+  };
+
+  const handleDeleteProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectProjectId),
+      };
+    });
+  };
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectProjectId
+  );
+  const selectedProjectTasks = projectsState.tasks.filter(
+    (task) => task.projectId === projectsState.selectProjectId
+  );
+
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={selectedProjectTasks}
+    />
+  );
+
+  if (projectsState.selectProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
+  } else if (projectsState.selectProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectProjectId}
+      />
+      {content}
+    </main>
   );
 }
 
---
+++ src/components/NewTask.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState("");
+
+  const handleChange = (event) => {
+    setEnteredTask(event.target.value);
+  };
+
+  const handleClick = () => {
+    if (enteredTask.trim() === "") return;
+
+    onAdd(enteredTask);
+    setEnteredTask("");
+  };
+
+  return (
+    <div className="flex items-center gap-4">
+      <input
+        onChange={handleChange}
+        value={enteredTask}
+        type="text"
+        className="w-64 px-2 rounded-sm bg-stone-200"
+      />
+      <button className="text-stone-700 hover:text-stone-950" onClick={handleClick}>
+        Add Task
+      </button>
+    </div>
+  );
+}
